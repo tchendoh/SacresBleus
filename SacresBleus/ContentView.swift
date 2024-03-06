610135e124baaa2dae410cd35999c9fc0c3c70ad
tchendoh <42325308+tchendoh@users.noreply.github.com>
2024-03-06 16:19:12 -0500

    Initial Commit

---
+++ SacresBleus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SacresBleus
+//
+//  Created by Eric Chandonnet on 2024-03-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SacresBleus/SacresBleusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SacresBleusApp.swift
+//  SacresBleus
+//
+//  Created by Eric Chandonnet on 2024-03-06.
+//
+
+import SwiftUI
+
+@main
+struct SacresBleusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
